8d58338adce759078d672637fe6ebfa8678e754e
Robert <rweitman@smu.edu>
2017-08-11 16:25:07 -0500

    add read

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# TriviaGame
+# Movie-Trivia
+
+
+![img](./assets/images/film_background.jpg)
+
+In this Movie Trivia Game, you can try to answer 7 movie questions within a time limit. When the time limit runs out the answers are not posted so there is still a challenge after it expires. 
+
+The movie questions are multiplechoice; they use radio type buttons. 
+
+The javascript file includes:
+
+-----A startTimer function that formats times
+-----Setting the target/countdown number
+-----Starting the startTimer
+-----Processing Results
+-----The Message After the Timer ends
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,8 @@
 var timing;
 
 
+// Formatting Timer into a Clock Format
+
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
     var entry = true;
@@ -28,18 +30,18 @@ function startTimer(duration, display) {
     }, 1000);
 }
 
-
+// Sets the total number of seconds available
 window.onload = function () {
-    var countdown = 10;
+    var countdown = 30;
 
-
+// Initiates the timer
 display = document.querySelector('#timer');
 
     startTimer(countdown, display);
 };
 
 
-
+// Checks to See if the Choices are Correct
 function endGame()
 {
   var wins=0;
@@ -77,14 +79,70 @@ for (i=0;i<ratings.length;i++)
 
 };
 
+ var stars = $(".star");
+
+for (i=0;i<stars.length;i++)
+{
+  if((stars[i].value === "correct") && stars[i].checked)
+   {
+    console.log("STAR WARS!");
+    wins++;
+   }
+
+};
+
+ var awards = $(".award");
+
+for (i=0;i<awards.length;i++)
+{
+  if((awards[i].value === "correct") && awards[i].checked)
+   {
+    console.log("AWARD SHOW!");
+    wins++;
+   }
+
+};
+
+ var directors = $(".director");
+
+for (i=0;i<directors.length;i++)
+{
+  if((directors[i].value === "correct") && directors[i].checked)
+   {
+    console.log("Spielberg the Director!");
+    wins++;
+   }
+
+};
 
 
-var z = "<h1>Totally Trivial Trivia!</h1><br><h2>You guessed " + wins + " correctly. You missed "  + (3-wins) +  " questions. </h2>";
 
+ var ices = $(".ice");
 
-$("#container").html(z) 
+for (i=0;i<ices.length;i++)
+{
+  if((ices[i].value === "correct") && ices[i].checked)
+   {
+    console.log("ICE AGE!");
+    wins++;
+   }
+
+};
+
+// Initiates new content into the body of the webpage
+var z = "<h1>Totally Trivial Trivia!</h1><br><h2>You guessed " + wins + " correctly. You missed "  + (7-wins) 
++  " questions. </h2><br><button id='restart'>Restart</button>";
+
+$("#container").html(z); 
+
+// This reloads the page when clicking an object that has an id "restart" 
+$("#restart").on("click", function(){
+      location.reload();
+      }); 
 
 console.log(wins);
 
-
 };
+
+
+
